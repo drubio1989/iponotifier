e393e2d33ecc3a447efbb27216d8014e6c6694bb
Daniel Rubio <drubio1989@gmail.com>
2024-01-04 15:23:29 -0600

    Debug once more initial deploy

--- pages/api/ipo-calendar.js
+++ pages/api/ipo-calendar.js
@@ -3,8 +3,9 @@
 import axios from 'axios';
 
 const getIpoCalendar = async () => {
+  console.log(`${process.env.NEXT_PUBLIC_IPO_CALENDAR_URL}/ipo-calendar`)
   try {
-    const response = await axios.get(`${process.env.IPO_CALENDAR_URL}/ipo-calendar`);
+    const response = await axios.get(`${process.env.NEXT_PUBLIC_IPO_CALENDAR_URL}/ipo-calendar`);
     const data = response.json();
     return data;
   } catch (error) {
